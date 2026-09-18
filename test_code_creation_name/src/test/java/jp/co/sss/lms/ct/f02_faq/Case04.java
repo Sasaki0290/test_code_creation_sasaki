6e88a1ccc8f502310eb72af87a14a104575e7c83
y-sasaki <y-sasaki@nexis.co.jp>
2026-09-18 15:27:00 +0900

    Case4の追加(3は前回追加してしまった)

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -82,6 +82,19 @@ public class Case04 {
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		WebElement menuLink = webDriver.findElement(By.cssSelector("a.dropdown-toggle"));
+
+		menuLink.click();
+
+		WebElement helpLink = webDriver.findElement(By.linkText("ヘルプ"));
+		String title = "ヘルプ | LMS";
+
+		helpLink.click();
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals(title, webDriver.getTitle());
 	}
 
 	@Test
@@ -89,6 +102,23 @@ public class Case04 {
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
 		// TODO ここに追加
+		WebElement questionLink = webDriver.findElement(By.linkText("よくある質問"));
+		String title = "よくある質問 | LMS";
+		int waitTime = 10;
+
+		questionLink.click();
+
+		// 別タブが開かれるのでそれらに対応するためのハンドル
+		Object[] windowHandles = webDriver.getWindowHandles().toArray();
+		// おそらく1は二つ目のタブに遷移する
+		webDriver.switchTo().window((String) windowHandles[1]);
+
+		visibilityTimeout(By.tagName("h2"), waitTime);
+
+		getEvidence(new Object() {
+		});
+
+		assertEquals(title, webDriver.getTitle());
 	}
 
 }
